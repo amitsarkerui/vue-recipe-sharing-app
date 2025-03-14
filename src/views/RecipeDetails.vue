bef656fc9ddd4326fb8282a0f1a491368bc4a12f
Amit Sarker <amitsarkerstock@gmail.com>
2025-03-15 00:19:46 +0600

    recipe details page

--- src/views/RecipeDetails.vue
+++ src/views/RecipeDetails.vue
@@ -4,39 +4,54 @@
       class="absolute h-[300px] inset-0 bg-[url(/pattern-home.jpg)] bg-cover bg-center"
     ></div>
     <div class="relative max-w-screen-xl px-4 mx-auto pt-14">
-      <div class="flex gap-7 flex-wrap">
-        <span class="flex gap-3 items-center">
-          <img class="h-12" src="/icons/prep.svg" alt="" />
-          <span>
-            <p class="text-xs text-primary">Preparation Time:</p>
-            <p class="font-semibold text-gray-700">
-              {{ recipe?.attributes?.prepTime }}
+      <div class="flex gap-8 items-center justify-between">
+        <div>
+          <h2 class="text-4xl pt-5">{{ recipe?.attributes?.title }}</h2>
+          <span class="flex gap-5 items-center mt-5">
+            <span class="flex gap-2 items-center">
+              <img
+                class="h-10 w-10 object-cover rounded-full"
+                :src="author?.attributes?.userImage?._url"
+                alt=""
+              />
+              <p class="text-gray-500">by : {{ author?.attributes?.name }}</p>
+            </span>
+            <div class="h-6 w-[1px] bg-gray-300"></div>
+            <p class="text-gray-500 mt-1">
+              Category : {{ recipe?.attributes?.category }}
             </p>
           </span>
-        </span>
-        <span class="flex gap-3 items-center">
-          <img class="h-12" src="/icons/cookTime.svg" alt="" />
-          <span>
-            <p class="text-xs text-primary">Cook Time:</p>
-            <p class="font-semibold text-gray-700">
-              {{ recipe?.attributes?.cookTime }}
-            </p>
+        </div>
+        <div class="flex gap-7 flex-wrap">
+          <span class="flex gap-3 items-center">
+            <img class="h-12" src="/icons/prep.svg" alt="" />
+            <span>
+              <p class="text-xs text-primary">Preparation Time:</p>
+              <p class="font-semibold text-gray-700">
+                {{ recipe?.attributes?.prepTime }}
+              </p>
+            </span>
           </span>
-        </span>
-        <span class="flex gap-3 items-center">
-          <img class="h-12" src="/icons/servings.svg" alt="" />
-          <span>
-            <p class="text-xs text-primary">Servings:</p>
-            <p class="font-semibold text-gray-700">
-              {{ recipe?.attributes?.servings }}
-            </p>
+          <span class="flex gap-3 items-center">
+            <img class="h-12" src="/icons/cookTime.svg" alt="" />
+            <span>
+              <p class="text-xs text-primary">Cook Time:</p>
+              <p class="font-semibold text-gray-700">
+                {{ recipe?.attributes?.cookTime }}
+              </p>
+            </span>
           </span>
-        </span>
+          <span class="flex gap-3 items-center">
+            <img class="h-12" src="/icons/servings.svg" alt="" />
+            <span>
+              <p class="text-xs text-primary">Servings:</p>
+              <p class="font-semibold text-gray-700">
+                {{ recipe?.attributes?.servings }}
+              </p>
+            </span>
+          </span>
+        </div>
       </div>
-      <h2 class="text-4xl pt-5">{{ recipe?.attributes?.title }}</h2>
-      <p class="text-gray-500 mt-3">
-        Category: {{ recipe?.attributes?.category }}
-      </p>
       <img
         class="mt-8 w-full h-[550px] object-cover"
         :src="recipe?.attributes?.image?._url"
@@ -88,12 +103,23 @@ export default {
     recipe() {
       return this.$store.getters.getRecipeById;
     },
+    author() {
+      return this.$store.getters.getUserById;
+    },
+  },
+  watch: {
+    recipe(newRecipe) {
+      if (newRecipe && newRecipe.attributes.relatedUser.id) {
+        this.$store.dispatch(
+          "FIND_USER_BY_ID",
+          this.recipe?.attributes?.relatedUser.id
+        );
+      }
+    },
   },
   mounted() {
-    this.$store.dispatch("FETCH_RECIPE_BY_ID", this.recipeId);
-  },
-  created() {
     this.recipeId = this.$route.params.id;
+    this.$store.dispatch("FETCH_RECIPE_BY_ID", this.recipeId);
   },
 };
 </script>
